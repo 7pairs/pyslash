d8180ede08340e372b7c4400afdcee69b015e9b2
Jun-ya HASEBA <7pairs@gmail.com>
2014-05-05 02:07:36 +0900

    Add test code #5

--- test/test_baseball.py
+++ test/test_baseball.py
@@ -54,3 +54,99 @@ def test_create_dict_02():
         html = test_file.read()
     actual = baseball.create_dict(html)
 
+
+def test_get_full_team_name_01():
+    """
+    引数に'西武'を指定したとき、'埼玉西武'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('西武')
+    assert_equal('埼玉西武', actual)
+
+
+def test_get_full_team_name_02():
+    """
+    引数に'楽天'を指定したとき、'東北楽天'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('楽天')
+    assert_equal('東北楽天', actual)
+
+
+def test_get_full_team_name_03():
+    """
+    引数に'ロッテ'を指定したとき、'千葉ロッテ'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('ロッテ')
+    assert_equal('千葉ロッテ', actual)
+
+
+def test_get_full_team_name_04():
+    """
+    引数に'ソフトバンク'を指定したとき、'福岡ソフトバンク'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('ソフトバンク')
+    assert_equal('福岡ソフトバンク', actual)
+
+
+def test_get_full_team_name_05():
+    """
+    引数に'オリックス'を指定したとき、'オリックス'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('オリックス')
+    assert_equal('オリックス', actual)
+
+
+def test_get_full_team_name_06():
+    """
+    引数に'日本ハム'を指定したとき、'北海道日本ハム'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('日本ハム')
+    assert_equal('北海道日本ハム', actual)
+
+
+def test_get_full_team_name_07():
+    """
+    引数に'巨人'を指定したとき、'読売'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('巨人')
+    assert_equal('読売', actual)
+
+
+def test_get_full_team_name_08():
+    """
+    引数に'阪神'を指定したとき、'阪神'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('阪神')
+    assert_equal('阪神', actual)
+
+
+def test_get_full_team_name_09():
+    """
+    引数に'広島'を指定したとき、'広島東洋'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('広島')
+    assert_equal('広島東洋', actual)
+
+
+def test_get_full_team_name_10():
+    """
+    引数に'中日'を指定したとき、'中日'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('中日')
+    assert_equal('中日', actual)
+
+
+def test_get_full_team_name_11():
+    """
+    引数に'ＤｅＮＡ'を指定したとき、'横浜ＤｅＮＡ'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('ＤｅＮＡ')
+    assert_equal('横浜ＤｅＮＡ', actual)
+
+
+def test_get_full_team_name_12():
+    """
+    引数に'ヤクルト'を指定したとき、'東京ヤクルト'を返すことを確認する。
+    """
+    actual = baseball.get_full_team_name('ヤクルト')
+    assert_equal('東京ヤクルト', actual)
+
