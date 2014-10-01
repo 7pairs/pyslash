9ac09735e522512fb36ea449f4c63e0dbace5c43
Jun-ya HASEBA <7pairs@gmail.com>
2014-10-01 10:19:58 +0900

    Refactor exception.py #18

--- crawler/exception.py
+++ crawler/exception.py
@@ -2,13 +2,18 @@
 
 
 class InvalidTeamError(Exception):
-    pass
+    """
+    指定されたチーム名が不正であることを示す例外。
+    """
 
 
 class ResultNotFoundError(Exception):
-    pass
+    """
+    指定された条件の試合が見つからないことを示す例外。
+    """
 
 
 class ParseError(Exception):
-    pass
-
+    """
+    文字列の解析に失敗したことを示す例外。
+    """
