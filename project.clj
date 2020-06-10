ff9b1a3fb674b4d69c06d35b8234237076a8d258
HASEBA Junya <7pairs@gmail.com>
2020-06-10 23:03:16 +0900

    Update project.clj #218

--- project.clj
+++ project.clj
@@ -1,7 +1,23 @@
-(defproject pyslash "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+; Copyright 2014-2020 HASEBA Junya
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+(defproject pyslash "2.0.0"
+  :description "Parse and format baseball scores."
+  :url "https://github.com/7pairs/pyslash"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:uberjar {:main pyslash.core, :aot :all}}
+  :main pyslash.core
   :repl-options {:init-ns pyslash.core})
