1cef5cfd0ae1a33cfc2238ee684688090c03e7f9
HASEBA Junya <7pairs@gmail.com>
2020-07-05 20:15:28 +0900

    Add test cases #78

---
+++ test/blue/lions/pyslash/util_test.clj
@@ -0,0 +1,21 @@
+; Copyright 2014-2020 HASEBA Junya
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+(ns blue.lions.pyslash.util-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [blue.lions.pyslash.util :as util]))
+
+(deftest test-index-of
+  (testing "3件目"
+    (is (= (util/index-of #(= % "山川") ["栗山" "岡田" "山川" "山野辺" "外崎"]) (list 2)))))
