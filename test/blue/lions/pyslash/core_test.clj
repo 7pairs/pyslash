f46445948f75d9b05d173755efc44563bfee7e2d
HASEBA Junya <7pairs@gmail.com>
2022-12-01 19:30:27 +0900

    Add test for first-element #234

--- test/blue/lions/pyslash/core_test.clj
+++ test/blue/lions/pyslash/core_test.clj
@@ -1,20 +1,49 @@
-; Copyright 2014-2022 HASEBA Junya
-;
-; Licensed under the Apache License, Version 2.0 (the "License");
-; you may not use this file except in compliance with the License.
-; You may obtain a copy of the License at
-;
-;     http://www.apache.org/licenses/LICENSE-2.0
-;
-; Unless required by applicable law or agreed to in writing, software
-; distributed under the License is distributed on an "AS IS" BASIS,
-; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-; See the License for the specific language governing permissions and
-; limitations under the License.
+;;;; Copyright 2014-2022 HASEBA Junya
+;;;;
+;;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;;; you may not use this file except in compliance with the License.
+;;;; You may obtain a copy of the License at
+;;;;
+;;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;;;
+;;;; Unless required by applicable law or agreed to in writing, software
+;;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;;; See the License for the specific language governing permissions and
+;;;; limitations under the License.
 
 (ns blue.lions.pyslash.core-test
   (:require [clojure.test :refer [deftest is testing]]
+            [net.cgrand.enlive-html :as html]
             [blue.lions.pyslash.core :as core]))
+
+(deftest test-for-first-element
+  (testing "セレクタにマッチする要素がノード内に存在する場合、その先頭の要素を返すこと。"
+    (let [node (html/html-snippet "<!DOCTYPE html>
+                                   <html>
+                                     <body>
+                                       <ul>
+                                         <li>栗山</li>
+                                         <li>中村</li>
+                                         <li>外崎</li>
+                                         <li>源田</li>
+                                       </ul>
+                                     </body>
+                                   </html>")]
+      (is (= (first (:content (core/first-element node [:ul :li]))) "栗山"))))
+  (testing "セレクタにマッチする要素がノード内に存在しない場合、nilを返すこと。"
+    (let [node (html/html-snippet "<!DOCTYPE html>
+                                   <html>
+                                     <body>
+                                       <ul>
+                                         <li>栗山</li>
+                                         <li>中村</li>
+                                         <li>外崎</li>
+                                         <li>源田</li>
+                                       </ul>
+                                     </body>
+                                   </html>")]
+      (is (nil? (core/first-element node [:ol :li]))))))
 
 (deftest test-get-formal-team-name
   (testing "埼玉西武ライオンズ"
